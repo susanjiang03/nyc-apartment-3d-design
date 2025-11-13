9d9b0b9e4625804e7255ca5f85480ea52cef6fd7
Colin Onevathana <business@onecolin.dev>
2025-11-12 20:23:41 -0600

    Add NYU Apartment layout with modern S-Chair & Bookshelf

---
+++ bookshelf.js
@@ -0,0 +1,84 @@
+// Generate bookshelf geometry (returns start and count for drawing)
+function GenerateBookshelf()
+{
+    var startIndex = points.length;
+    
+    var cube = [
+        vec4(-0.5, -0.5, 0.5, 1.0),
+        vec4(-0.5, 0.5, 0.5, 1.0),
+        vec4(0.5, 0.5, 0.5, 1.0),
+        vec4(0.5, -0.5, 0.5, 1.0),
+        vec4(-0.5, -0.5, -0.5, 1.0),
+        vec4(-0.5, 0.5, -0.5, 1.0),
+        vec4(0.5, 0.5, -0.5, 1.0),
+        vec4(0.5, -0.5, -0.5, 1.0)
+    ];
+    
+    // Add cube faces
+    quad(cube[1], cube[0], cube[3], cube[2]); // front
+    quad(cube[2], cube[3], cube[7], cube[6]); // right
+    quad(cube[3], cube[0], cube[4], cube[7]); // bottom
+    quad(cube[6], cube[5], cube[1], cube[2]); // top
+    quad(cube[4], cube[5], cube[6], cube[7]); // back
+    quad(cube[5], cube[4], cube[0], cube[1]); // left
+    
+    return {start: startIndex, count: points.length - startIndex};
+}
+
+// Draw the complete bookshelf at specified position
+function DrawBookshelf(x, y, z)
+{
+    var brown = vec4(0.6, 0.45, 0.3, 1);
+    gl.uniform4fv(gl.getUniformLocation(program, "ambientProduct"), flatten(brown));
+
+    var width = 2;
+    var height = 3;
+    var depth = 1;
+    var thickness = 0.05;
+
+    // 1. Left side panel
+    DrawBookshelfComponent(x - width/2 + thickness/2, y + height/2, z, thickness, height, depth);
+
+    // 2. Right side panel
+    DrawBookshelfComponent(x + width/2 - thickness/2, y + height/2, z, thickness, height, depth);
+
+    // 3. Top panel
+    DrawBookshelfComponent(x, y + height - thickness/2, z, width, thickness, depth);
+
+    // 4. Bottom panel
+    DrawBookshelfComponent(x, y + thickness/2, z, width, thickness, depth);
+
+    // 5. Back panel (thinner)
+    DrawBookshelfComponent(x, y + height/2, z - depth/2 + thickness/4, width - thickness*2, height - thickness*2, thickness*0.5);
+
+    // 6. First shelf
+    var shelf1Y = y + height * 0.25;
+    DrawBookshelfComponent(x, shelf1Y, z, width - thickness*2, thickness, depth);
+
+    // 7. Second shelf
+    var shelf2Y = y + height * 0.5;
+    DrawBookshelfComponent(x, shelf2Y, z, width - thickness*2, thickness, depth);
+
+    // 8. Third shelf
+    var shelf3Y = y + height * 0.75;
+    DrawBookshelfComponent(x, shelf3Y, z, width - thickness*2, thickness, depth);
+
+    // 9. Left vertical divider (between shelves 1 and 2) - moved toward center
+    var divider1Height = shelf2Y - shelf1Y;
+    DrawBookshelfComponent(x - (width - thickness*2)/4, (shelf1Y + shelf2Y) / 2, z, thickness*0.8, divider1Height, depth);
+
+    // 10. Right vertical divider (between shelves 2 and 3) - moved toward center
+    var divider2Height = shelf3Y - shelf2Y;
+    DrawBookshelfComponent(x + (width - thickness*2)/4, (shelf2Y + shelf3Y) / 2, z, thickness*0.8, divider2Height, depth);
+}
+
+function DrawBookshelfComponent(x, y, z, w, h, d)
+{
+    modelViewStack.push(modelViewMatrix);
+    var s = scale4(w, h, d);
+    var t = translate(x, y, z);
+    modelViewMatrix = mult(mult(modelViewMatrix, t), s);
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+    gl.drawArrays(gl.TRIANGLES, bookshelfGeometry.start, bookshelfGeometry.count);
+    modelViewMatrix = modelViewStack.pop();
+}
---
+++ modernChair.js
@@ -0,0 +1,123 @@
+// S-Chair: Base, Diagonal, Seat, Back
+function GenerateChair()
+{
+    var startIndex = points.length;
+    
+    var width = 0.75;
+    var thickness = 0.1;
+    
+    // Base section
+    var base_back = 0.6;
+    var base_height = 0;
+    
+    // Diagonal section
+    var diag_start = base_back - .6;
+    var diag_end = base_back;
+    var diag_bottom = base_height;
+    var diag_top = 0.5;
+    
+    // Seat section
+    var seat_front = diag_end - .7;
+    var seat_back = diag_end;
+    var seat_height = diag_top;
+    
+    // Back section
+    var back_bottom = seat_back - .7;
+    var back_top = seat_back - .8;
+    var back_height_bottom = seat_height;
+    var back_height_top = seat_height + 0.6;
+    
+    // BASE
+    var base_vertices = [
+        vec4(-width/2, base_height, 0, 1.0),
+        vec4(width/2, base_height, 0, 1.0),
+        vec4(-width/2, base_height, base_back, 1.0),
+        vec4(width/2, base_height, base_back, 1.0),
+        vec4(-width/2, base_height + thickness, 0, 1.0),
+        vec4(width/2, base_height + thickness, 0, 1.0),
+        vec4(-width/2, base_height + thickness, base_back, 1.0),
+        vec4(width/2, base_height + thickness, base_back, 1.0)
+    ];
+    
+    quad(base_vertices[0], base_vertices[1], base_vertices[3], base_vertices[2]);
+    quad(base_vertices[6], base_vertices[7], base_vertices[5], base_vertices[4]);
+    quad(base_vertices[2], base_vertices[3], base_vertices[7], base_vertices[6]);
+    quad(base_vertices[4], base_vertices[5], base_vertices[1], base_vertices[0]);
+    quad(base_vertices[0], base_vertices[2], base_vertices[6], base_vertices[4]);
+    quad(base_vertices[5], base_vertices[7], base_vertices[3], base_vertices[1]);
+    
+    // DIAGONAL
+    var diag_vertices = [
+        vec4(-width/2, diag_bottom + thickness, diag_start, 1.0),
+        vec4(width/2, diag_bottom + thickness, diag_start, 1.0),
+        vec4(-width/2, diag_top, diag_end, 1.0),
+        vec4(width/2, diag_top, diag_end, 1.0),
+        vec4(-width/2, diag_bottom + thickness*2, diag_start, 1.0),
+        vec4(width/2, diag_bottom + thickness*2, diag_start, 1.0),
+        vec4(-width/2, diag_top + thickness, diag_end, 1.0),
+        vec4(width/2, diag_top + thickness, diag_end, 1.0)
+    ];
+    
+    quad(diag_vertices[0], diag_vertices[1], diag_vertices[3], diag_vertices[2]);
+    quad(diag_vertices[6], diag_vertices[7], diag_vertices[5], diag_vertices[4]);
+    quad(diag_vertices[2], diag_vertices[3], diag_vertices[7], diag_vertices[6]);
+    quad(diag_vertices[4], diag_vertices[5], diag_vertices[1], diag_vertices[0]);
+    quad(diag_vertices[0], diag_vertices[2], diag_vertices[6], diag_vertices[4]);
+    quad(diag_vertices[5], diag_vertices[7], diag_vertices[3], diag_vertices[1]);
+    
+    // SEAT
+    var seat_vertices = [
+        vec4(-width/2, seat_height, seat_front, 1.0),
+        vec4(width/2, seat_height, seat_front, 1.0),
+        vec4(-width/2, seat_height, seat_back, 1.0),
+        vec4(width/2, seat_height, seat_back, 1.0),
+        vec4(-width/2, seat_height + thickness, seat_front, 1.0),
+        vec4(width/2, seat_height + thickness, seat_front, 1.0),
+        vec4(-width/2, seat_height + thickness, seat_back, 1.0),
+        vec4(width/2, seat_height + thickness, seat_back, 1.0)
+    ];
+    
+    quad(seat_vertices[0], seat_vertices[1], seat_vertices[3], seat_vertices[2]);
+    quad(seat_vertices[6], seat_vertices[7], seat_vertices[5], seat_vertices[4]);
+    quad(seat_vertices[2], seat_vertices[3], seat_vertices[7], seat_vertices[6]);
+    quad(seat_vertices[4], seat_vertices[5], seat_vertices[1], seat_vertices[0]);
+    quad(seat_vertices[0], seat_vertices[2], seat_vertices[6], seat_vertices[4]);
+    quad(seat_vertices[5], seat_vertices[7], seat_vertices[3], seat_vertices[1]);
+    
+    // BACK
+    var back_vertices = [
+        vec4(-width/2, back_height_bottom + thickness, back_bottom, 1.0),
+        vec4(width/2, back_height_bottom + thickness, back_bottom, 1.0),
+        vec4(-width/2, back_height_top, back_top, 1.0),
+        vec4(width/2, back_height_top, back_top, 1.0),
+        vec4(-width/2, back_height_bottom + thickness*2, back_bottom, 1.0),
+        vec4(width/2, back_height_bottom + thickness*2, back_bottom, 1.0),
+        vec4(-width/2, back_height_top + thickness, back_top, 1.0),
+        vec4(width/2, back_height_top + thickness, back_top, 1.0)
+    ];
+    
+    quad(back_vertices[0], back_vertices[1], back_vertices[3], back_vertices[2]);
+    quad(back_vertices[6], back_vertices[7], back_vertices[5], back_vertices[4]);
+    quad(back_vertices[2], back_vertices[3], back_vertices[7], back_vertices[6]);
+    quad(back_vertices[4], back_vertices[5], back_vertices[1], back_vertices[0]);
+    quad(back_vertices[0], back_vertices[2], back_vertices[6], back_vertices[4]);
+    quad(back_vertices[5], back_vertices[7], back_vertices[3], back_vertices[1])
+    
+    return {start: startIndex, count: points.length - startIndex};
+}
+
+
+
+// Draw the S-Chair at specified position
+function DrawChair(x, y, z)
+{
+    var chairColor = vec4(0.9, 0.3, 0.3, 1);
+    gl.uniform4fv(gl.getUniformLocation(program, "ambientProduct"), flatten(chairColor));
+    
+    modelViewStack.push(modelViewMatrix);
+    var t = translate(x, y, z);
+    modelViewMatrix = mult(modelViewMatrix, t);
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+    gl.drawArrays(gl.TRIANGLES, chairGeometry.start, chairGeometry.count);
+    modelViewMatrix = modelViewStack.pop();
+}
